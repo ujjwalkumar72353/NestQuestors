4c3f6e94a7d7fe8741b8c30c296f1a588f2e7a51
ujjwalkumar7498 <ujjwalkumar4876@gmail.com>
2024-06-18 23:40:41 +0530

    create api signup route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,3 +1,15 @@
-export const signup = (req,res)=>{
-      console.log(req.body);
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async(req,res)=>{
+      const {username,email,password}=req.body;
+      const hashpassword = bcryptjs.hashSync(password,10);
+      const newuser=new User({username,email,password:hashpassword });
+      try{
+      await newuser.save();    
+      res.status(201).json("user created sucessfully");
+      }
+      catch(err){
+        res.status(500).json(err.message);
+      }
 }
--- api/index.js
+++ api/index.js
@@ -14,6 +14,8 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000,()=>{
     console.log('server is running at port 3000!!');
 })
