87e90b33437016728e3de7a8fa3dd847b29acd17
ujjwalkumar7498 <ujjwalkumar4876@gmail.com>
2024-06-22 09:27:02 +0530

    create redux store and userSlice

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,12 +1,18 @@
 // eslint-disable-next-line no-unused-vars
 import { useState } from 'react';
+import { useDispatch ,useSelector} from 'react-redux';
 import { Link,useNavigate} from 'react-router-dom';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
 
-export default function Signin() {
+export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -16,7 +22,7 @@ export default function Signin() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -27,18 +33,15 @@ export default function Signin() {
       const data = await res.json();
       console.log(data);
       if (data.success === false) {
-        setLoading(false);
-        setError(data.message);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
+      dispatch(signInSuccess(data));
       navigate('/');
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFailure(error.message));
     }
-  }
+  };
 
   return (
     <div className='p-3 max-w-lg mx-auto mt-10'>
