57666b556d111a8853ad427b057f3813b1377042
ujjwalkumar7498 <ujjwalkumar4876@gmail.com>
2024-06-22 17:57:43 +0530

    complete profile page ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,10 +1,27 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Profile = () => {
+  const {currentUser}=useSelector(state=>state.user)
   return (
-    <div>
-      Profile
+    <div className='p-3 max-w-lg mx-auto'>
+     <h1 className='text-3xl text-center font-semibold my-7'>Profile</h1>
+     <form className='flex flex-col gap-4'>
+      <img className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' src={currentUser.avatar} alt='profile'/>
+      <input type='text' placeholder='Username' id='username' className='border p-3 rounded-lg  '/>
+      <input type='text' placeholder='Email' id='username' className='border p-3 rounded-lg '/>
+      <input type='text' placeholder='Password' id='username' className='border p-3 rounded-lg '/>
+      <button className='bg-slate-700 p-3 text-white uppercase rounded-lg hover:opacity-95 disabled:opacity-80'>Update</button>
+     </form>
+     <div className='flex justify-between mt-5'>
+     <span className='text-orange-700 cursor-pointer'>
+     Delete account
+     </span>
+     <span  className='text-orange-700 cursor-pointer'>
+      Sign out
+     </span>
+     </div>
     </div>
   )
 }
