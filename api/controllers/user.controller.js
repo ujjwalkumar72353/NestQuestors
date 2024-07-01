681fc1042737d91d3fc5521d5085110ab2662eb5
ujjwalkumar7498 <ujjwalkumar4876@gmail.com>
2024-07-01 09:52:21 +0530

    create get user listing api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
 import { errorHandler } from '../utilis/error.js';
+import Listing from '../models/listing.model.js';
 export const test = (req, res) => {
   res.json({
     message: 'Api route is working!',
@@ -8,7 +9,7 @@ export const test = (req, res) => {
 };
 
 export const updateUser = async (req, res, next) => {
-  if (req.user.id !== req.params.id)
+  if (req.user.id === req.params.id)
     return next(errorHandler(401, 'You can only update your own account!'));
   try {
     if (req.body.password) {
@@ -47,3 +48,17 @@ export const deleteUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getUserListings = async (req, res, next) => {
+  if(req.user.id1=req.params.id){
+  try {
+    const listings = await Listing.find({useRef:req.params.id});
+    res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+}
+else {
+  return next(errorHandler(401, 'You can only delete your own account!'));
+}
+}
