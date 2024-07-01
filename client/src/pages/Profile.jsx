ed2f84fe384775abac2e82d042bf1842972786a9
ujjwalkumar7498 <ujjwalkumar4876@gmail.com>
2024-07-01 15:48:32 +0530

    complete delete user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -276,12 +276,12 @@ export default function Profile() {
               </Link>
 
               <div className='flex flex-col item-center'>
-                <button
-                  onClick={() => handleListingDelete(listing._id)}
-                  className='text-red-700 uppercase'
-                >
-                  Delete
-                </button>
+              <button
+              onClick={() => handleListingDelete(listing._id)}
+              className='text-red-700 uppercase'
+            >
+              Delete
+            </button>
                 <Link to={`/update-listing/${listing._id}`}>
                   <button className='text-green-700 uppercase'>Edit</button>
                 </Link>
